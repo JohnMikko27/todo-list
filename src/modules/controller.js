e24c0499b40c8211f65eb81b8a4680c46d88b833
Mikko <realmiks27@gmail.com>
2023-08-18 15:29:02 -0700

    Style left side and top

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -1,6 +1,7 @@
 //handles if a DOM changing (ui.js) and applying the correct login from todo.js
 //for example, if this button is clicked (an eventListener) it will run this logic from todo.js then change the DOM accordingly in ui.js
-import { displayDefaultProject, addProject, addTask, allTasksClicked, todayTasksClicked, futureTodosClicked, displayProjects, clearProjectHeader } from "./ui";
+import { displayDefaultProject, addTask, allTasksClicked, todayTasksClicked, futureTodosClicked } from "./ui";
+import { addProject, displayProjects, clearProjectHeader } from "./projectUI"
 import { onload } from "./localStorageUI";
 
 
